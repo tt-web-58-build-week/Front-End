ef3d04d91b2608d90cd38f98a44bf57c52aca40b
Xunxin Li <xli9400@Xunxins-MBP.lan1>
2021-04-26 19:28:33 -0400

    Complete the content components for home page

---
+++ secret-family-recipes/src/components/Company.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+const Company = (prop)=>{
+    return (
+        <div>
+            {/* img info is incomplete */}
+            <img></img>
+            <h1>
+                <strong>Secret Family Recipes</strong>, your place for
+                <br></br>
+                generation after generation of tradition
+            </h1>
+            <ul>
+                {/* ########################icon css is incomplete############################# */}
+                <li>
+                    <a href='#' className="icon brands fa-twitter"></a>
+                </li>
+                <li>
+                    <a href='#' className="icon brands fa-github"></a>
+                </li>
+                <li>
+                    <a href='#' className="icon brands fa-dribble"></a>
+                </li>
+                <li>
+                    <a href='#' className="icon brands fa-envelope"></a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Company;
