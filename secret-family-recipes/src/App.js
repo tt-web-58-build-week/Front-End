ec46e0615d574712e1b34a2e0d5e7e35224c0e3e
Cooper Jackson <davisjaxon@gmail.com>
2021-04-26 20:51:11 -0700

    got modals working, just need to start making api calls and styling

---
+++ secret-family-recipes/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import LoginModal from './LoginModal'
+import SignUpModal from './SignUpModal'
+import RecipeModal from './RecipeModal'
+import Modal from 'react-modal'
+import React, {useState} from 'react'
+
+Modal.setAppElement('#root')
+function App() {
+  const [loginModalIsOpen, setLoginModalIsOpen] = useState(false)
+  const [signUpModalIsOpen, setSignUpModalIsOpen] = useState(false)
+  const [recipeModalIsOpen, setRecipeModalIsOpen] = useState(false)
+
+
+
+    return (
+      <div>
+        <button onClick={()=> setLoginModalIsOpen(true)}>Open Login</button>
+        <LoginModal modalIsOpen={loginModalIsOpen} setModalIsOpen={setLoginModalIsOpen}/>
+        <button onClick={()=> setSignUpModalIsOpen(true)}>Open Sign Up</button>
+        <SignUpModal modalIsOpen={signUpModalIsOpen} setModalIsOpen={setSignUpModalIsOpen}/>
+        <button onClick={()=> setRecipeModalIsOpen(true)}>Open Recipes</button>
+        <RecipeModal modalIsOpen={recipeModalIsOpen} setModalIsOpen={setRecipeModalIsOpen}/>
+      </div>
+    );
+}
+
+export default App;
+
+
+  // return (
+  //   <div className="App">
+  //     <button onClick={()=> setModalIsOpen(true)}>Open Modal</button>
+  //     <LoginModal modalIsOpen={modalIsOpen}/>
+  //   </div>
+  // );
---
+++ secret-family-recipes/src/RecipeModal.js
@@ -0,0 +1,95 @@
+import React,{useState} from 'react';
+import Modal from 'react-modal'
+import './RecipeModal.css'
+
+export default function RecipeModal(props) {
+    const {modalIsOpen, setModalIsOpen} = props
+    const [formValues, setFormValues] = useState({})
+
+    const onChange = event => {
+        const {name, value } = event.target
+        setFormValues({...formValues, [name]: value}) 
+    }
+    
+    const onSubmit = event => {
+        event.preventDefault();
+        setModalIsOpen(false)
+    }
+
+    const addIngredient = event => {
+        event.preventDefault();
+        const ingredients = document.querySelector('#ingredients')
+        const newIngredient = document.createElement('input')
+        ingredients.appendChild(newIngredient)
+    }
+
+    return (
+        <Modal isOpen={modalIsOpen} 
+        onRequestClose={() => setModalIsOpen(false)}
+        portalClassName='modal'
+        style={
+            {
+                overlay: {
+                    backgroundColor: 'rgba(0,0,0,.7)'
+                },
+                content: {
+                    margin: 'auto',
+                    color: 'orange',
+                    minWidth: '20em',
+                    width: '30%',
+                    minHeight: '15%',
+                    maxHeight: '40%',
+                },
+            }
+        }>
+            <button style={{float: 'right'}} onClick={()=> setModalIsOpen(false)}>X</button>
+            <h2>Add Recipe</h2>
+            <br/>
+            <form onSubmit={onSubmit}>
+                <div>
+
+                <label>Title: </label>
+                <input 
+                    name="title"
+                    type="text"
+                    onChange={onChange}
+                />
+                </div>
+                <div>
+                <label>Source: </label>
+                <input 
+                    name="source"
+                    type="text"
+                    onChange={onChange}
+                />
+                </div>
+                <div id="ingredients">
+                <label>Ingredient: </label>
+                <input 
+                    name="title"
+                    type="text"
+                    onChange={onChange}
+                />
+                <button onClick={addIngredient}>YEE</button>
+                </div>
+                <div>
+                <label>Instructions: </label>
+                <input 
+                    name="title"
+                    type="text"
+                    onChange={onChange}
+                />
+                </div>
+                <div>
+                    <label>Category: </label>
+                    <input 
+                        name="Category"
+                        type="text"
+                        onChange={onChange}
+                    />
+                </div>
+                <button id='submit-button'>Submit Recipe</button>
+            </form>
+        </Modal>
+    )
+}
