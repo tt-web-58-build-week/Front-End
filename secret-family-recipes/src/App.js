459dd5d95a7a0c058287b34de9e5290b599ebbc1
Xunxin Li <xli9400@Xunxins-MacBook-Pro.local>
2021-04-27 16:47:40 -0400

    Got the functionailty to display datas

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 import About from "./components/About";
 import Company from "./components/Company";
 import Recipe from "./components/Recipe";
 import Nav from "./components/NavBar";
+import axios from "axios";
+
+
+const MOCK_DATA = [
+  {
+      recipeid: 1,
+      source: 'example',
+      title: 'Recipe1',
+      categories: [{categoryid: 1, name: 'example1'}, {categoryid: 2, name: 'example2'}],
+      ingredients: [{ingredientid: 1, ingredientname: 'example1'}, {ingredientid: 2, ingredientname: 'example2'}, {ingredientid: 3, ingredientname: 'example3'}, {ingredientid: 4, ingredientname: 'example4'}],
+      instructions: [{instructionsid: 1, instructionDetails: 'sample instructions 1'}, {instructionsid: 2, instructionDetails: 'sample instructions 2'}],
+      user: {email: 'jeff@jeff.jeff', password: '1234', passwordNoEncrypt: '1234', userid: 1, username: 'jeff'}
+  },
+  {
+      recipeid: 2,
+      source: 'example',
+      title: 'Recipe2',
+      categories: [{categoryid: 1, name: 'example1'}, {categoryid: 2, name: 'example2'}],
+      ingredients: [{ingredientid: 1, ingredientname: 'example1'}, {ingredientid: 2, ingredientname: 'example2'}, {ingredientid: 3, ingredientname: 'example3'}, {ingredientid: 4, ingredientname: 'example4'}],
+      instructions: [{instructionsid: 1, instructionDetails: 'sample instructions 1'}, {instructionsid: 2, instructionDetails: 'sample instructions 2'}],
+      user: {email: 'jeff@jeff.jeff', password: '1234', passwordNoEncrypt: '1234', userid: 1, username: 'jeff'}
+  },
+  {
+      recipeid: 3,
+      source: 'example',
+      title: 'Recipe3',
+      categories: [{categoryid: 1, name: 'example1'}, {categoryid: 2, name: 'example2'}],
+      ingredients: [{ingredientid: 1, ingredientname: 'example1'}, {ingredientid: 2, ingredientname: 'example2'}, {ingredientid: 3, ingredientname: 'example3'}, {ingredientid: 4, ingredientname: 'example4'}],
+      instructions: [{instructionsid: 1, instructionDetails: 'sample instructions 1'}, {instructionsid: 2, instructionDetails: 'sample instructions 2'}],
+      user: {email: 'jeff@jeff.jeff', password: '1234', passwordNoEncrypt: '1234', userid: 1, username: 'jeff'}
+  },
+  {
+      recipeid: 4,
+      source: 'example',
+      title: 'Recipe4',
+      categories: [{categoryid: 1, name: 'example1'}, {categoryid: 2, name: 'example2'}],
+      ingredients: [{ingredientid: 1, ingredientname: 'example1'}, {ingredientid: 2, ingredientname: 'example2'}, {ingredientid: 3, ingredientname: 'example3'}, {ingredientid: 4, ingredientname: 'example4'}],
+      instructions: [{instructionsid: 1, instructionDetails: 'sample instructions 1'}, {instructionsid: 2, instructionDetails: 'sample instructions 2'}],
+      user: {email: 'jeff@jeff.jeff', password: '1234', passwordNoEncrypt: '1234', userid: 1, username: 'jeff'}
+  }
+];
 
 function App() {
+  const [ourFavoriteRecipes, setOurFavoriteRecipes] = useState(MOCK_DATA);
+
+  // axios.get(url)
+  // .then(res=>{
+  //   setOurFavoriteRecipes(res.data);
+  // })
+  // .catch(error=>{
+  //   console.log("Something went wrong");
+  // })
+  
   return (
     <section>
       <div>
@@ -15,7 +66,11 @@ function App() {
         <About/>
         <section>
             <h2>Some of our favorite recipes</h2>
-            <Recipe/>
+            {
+              ourFavoriteRecipes.map(recipe=>{
+                return <Recipe key={recipe.recipeid} recipe={recipe}/>
+              })
+            }
         </section>
       </div>
     </section>
--- secret-family-recipes/src/components/Recipe.js
+++ secret-family-recipes/src/components/Recipe.js
@@ -1,19 +1,22 @@
 import React from "react";
 
 
-
 const Recipe = (prop)=>{
-    
+    const {recipe} = prop;
     return (
         <article>
-            <a></a>
-            <h3></h3>
-            <p></p>
+            <a>
+                <img src="https://via.placeholder.com/250"></img>
+            </a>
+            <h3>{`${recipe.title} from ${recipe.source}`}</h3>
+            <p>Ingredients: {recipe.ingredients.map(ingredient=>{
+                if(recipe.ingredients.indexOf(ingredient)!== recipe.ingredients.length-1){
+                    return `${ingredient.ingredientname}, `;
+                }
+                return ingredient.ingredientname;
+            })}</p>
         </article>
     )
-
-
 }
-
 
 export default Recipe;
