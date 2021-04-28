60b1d9dc5cccad5c53b1ad4162c93bed06ca4a77
Jack Henry <jackson.lewis.henry@gmail.com>
2021-04-28 19:03:34 -0400

    pair programming changes with cory and jack- login still returning 401

--- secret-family-recipes/src/components/LoginModal.js
+++ secret-family-recipes/src/components/LoginModal.js
@@ -1,32 +1,91 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Modal from 'react-modal';
-import axios from "axios"
+import * as yup from 'yup';
+import { useHistory } from 'react-router';
+
+const INITIAL_FORM_VALUES = {
+    username: '',
+    password: ''
+} 
+
+const INITIAL_FORM_ERRORS = {
+    username: '',
+    password: ''
+}
+
+const schema = yup.object().shape({
+    username: yup.string().required('required'),
+    password: yup.string().required('required')
+})
+
 
 export default function LoginModal(props) {
-    const {modalIsOpen, setModalIsOpen} = props
-    const [formValues, setFormValues] = useState({})
+    const {modalIsOpen, setModalIsOpen, submit, setUserID } = props
+    const [values, setValues] = useState(INITIAL_FORM_VALUES);
+    const [errors, setErrors] = useState(INITIAL_FORM_ERRORS)
+    const [disabled, setDisabled] = useState(true)
+    const { push } = useHistory();
 
-    const apiURL = "https://tt-web58-recipe-app.herokuapp.com/swagger-ui.html"
+
+
+
+
+    // const apiURL = "https://tt-web58-recipe-app.herokuapp.com/api/"
+
+    const onSubmit = event => {
+        event.preventDefault()
+        console.log(values)
+        schema.validate(values)
+        .then(_ => {
+        submit(values, push, setUserID);
+        setModalIsOpen(false)
+        setValues(INITIAL_FORM_VALUES);
+        setErrors(INITIAL_FORM_ERRORS) 
+        })
+        .catch(err => {
+            console.error(err)
+        })       
+    }
 
     const onChange = event => {
         const {name, value} = event.target
-        setFormValues({...formValues, [name]: value})
+        setValues({
+            ...values,
+             [name]: value
+            })
+        //could insert validation schema here
+
+        yup.reach(schema, name)
+            .validate(value)
+            .then(_ => {
+                setErrors({
+                    ...errors,
+                    [name]: ''
+                })
+            })
+            .catch(err => {
+                setErrors({ 
+                    ...errors,
+                    [name]: err.errors[0]
+                })
+                    setDisabled(true)
+            })
     }
 
-    const onSubmit = event => {
-        event.preventDefault();
-        setModalIsOpen(false)
-    }
-
-    const doIt = () => {
-        axios.get(apiURL)
-        .then(res => {
-            console.log(res)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
+    useEffect(() => {
+        schema.validate(values)
+            .then(isValid => setDisabled(!isValid))
+            .catch(err => console.log(err))
+    })
+    // const doIt = () => {
+    //     axios.get(apiURL)
+    //     .then(res => {
+    //         console.log(res)
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
+    // }
 
     return (
         <Modal isOpen={modalIsOpen} 
@@ -52,25 +111,29 @@ export default function LoginModal(props) {
                 <h2>User Login</h2>
                 <br/>
                 <div>
-                    <label>Email: </label>
+                    <label>Username: </label>
                     <input
-                        name='email'
-                        type='text'
                         onChange={onChange}
-                    />
+                        value={values.username}
+                        name='username'
+                        type='text'
+                        error={errors.username}
+                        />
                     <br/>
 
                     <label>Password: </label>
                         <input
-                        name='password'
-                        type='text'
                         onChange={onChange}
-                    />
+                        value={values.password}
+                        name='password'
+                        type='password'
+                        error={errors.password}
+                        />
 
-                    <button id='submit-button'>Login</button>
+                    <button variant={disabled ? 'disabled' : 'success'}>Login</button>
                 </div>
             </form>
-            <button id="do-it-button" onClick={doIt}>console log the users api</button>
+            {/* <button id="do-it-button" onClick={doIt}>console log the users api</button> */}
         </Modal>
     )
 }
